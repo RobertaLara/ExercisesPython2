1e6620096e6863b178ed7dee1d3ad8f5d13dcd10
RobertaLara <61847180+RobertaLara@users.noreply.github.com>
2020-05-20 17:18:51 +0200

    Create Exercise00.py

---
+++ Exercise00.py
@@ -0,0 +1,9 @@
+# Exercício 00 - Faça um Programa que mostre a mensagem "Alo mundo" na tela
+# Programadora Roberta Lara
+
+import os          
+def clear():            # Função para limpar console
+    os.system('clear')
+clear()
+
+print("\n\n\tAlo mundo\n\n")        # print é usado para exibir frase na tela
