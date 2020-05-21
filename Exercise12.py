5a7d0e7d9041644e17ef4c1a7276c7727e70a748
RobertaLara <61847180+RobertaLara@users.noreply.github.com>
2020-05-21 11:35:15 +0200

    Create Exercise12.py

---
+++ Exercise12.py
@@ -0,0 +1,21 @@
+# Exercício 12 - Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+        # Para homens: (72.7*h) - 58
+        # Para mulheres: (62.1*h) - 44.7
+# Programadora Roberta Lara
+
+import os          
+def clear():            # Função para limpar console
+    os.system('clear')
+clear()
+
+altura = input("\n\nDigite a sua altura: ")
+sexo = input("\nQual o seu sexo?"
+            "\n\tPara homem, digite 1"
+            "\n\tPara mulher, digite qualquer outro valor\n")
+
+if (sexo == '1'):
+    pesoIdeal = round(((float(altura) * 72.7) - 58),3)      # round mostra a quantidade de casas decimais que eu desejar
+elif (sexo != '1'):
+    pesoIdeal = round(((float(altura) * 62.1) - 44.7),3)
+
+print("\n\n\tCom base na sua altura e sexo, seu peso ideal é: ",pesoIdeal,"\n\n\n")
