35d88bdd328472fe4e03a184b9751ff47441ca2c
RobertaLara <61847180+RobertaLara@users.noreply.github.com>
2020-05-21 10:51:49 +0200

    Create Exercise09.py

---
+++ Exercise09.py
@@ -0,0 +1,13 @@
+# Exercício 08 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit
+# Programadora Roberta Lara
+
+import os          
+def clear():            # Função para limpar console
+    os.system('clear')
+clear()
+
+celsius = input("\n\nDigite a temperatura em graus Celsius: ")
+
+farenheit = round((float(celsius) * 9 / 5 + 32),2)
+
+print("\n\n\tA temperatura convertida em Farenheit é: ",farenheit,"\n\n")
